8e604c26566ee753eda6dc9b8c93fb7309b71f7e
lstuma <lstuma@lstuma.dev>
2025-05-23 11:15:19 +0200

    frontend changes

--- dashboard/frontend/src/api.jsx
+++ dashboard/frontend/src/api.jsx
@@ -1,13 +1,26 @@
+import { toast } from "react-toastify";
+
 const API_BASE = "http://localhost:8080";
 
-class Service {}
+class Service {
+  constructor(name, status, description, port) {
+    this.name = name;
+    this.status = status;
+    this.description = description;
+    this.port = port;
+  }
+}
 
 async function request(path) {
   return (await fetch(API_BASE + path)).text();
 }
 
 async function list_services() {
-  return JSON.parse(await request("/services/list"));
+  const data = JSON.parse(await request("/services/list"));
+  const services = Object.entries(data).map(([name, data]) => {
+    return new Service(name, data["status"], data["description"], data["port"]);
+  });
+  return services;
 }
 
 async function get_service_status(service_name) {
@@ -15,14 +28,17 @@ async function get_service_status(service_name) {
 }
 
 async function start_service(service_name) {
+  toast(`Starting ${service_name}`, { hideProgressBar: true });
   return JSON.parse(await request(`/services/${service_name}/start`));
 }
 
 async function stop_service(service_name) {
+  toast(`Stopping ${service_name}`, { hideProgressBar: true });
   return JSON.parse(await request(`/services/${service_name}/stop`));
 }
 
 async function restart_service(service_name) {
+  toast(`Restarting ${service_name}`, { hideProgressBar: true });
   return JSON.parse(await request(`/services/${service_name}/restart`));
 }
 
--- dashboard/frontend/src/components/sidebar.jsx
+++ dashboard/frontend/src/components/sidebar.jsx
@@ -1,22 +1,35 @@
 import "./sidebar.css";
 import { Link, useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { FaCircleCheck, FaHouse } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
+import { BsGearWide } from "react-icons/bs";
+import { AiFillMessage } from "react-icons/ai";
+import { MdDashboard } from "react-icons/md";
 
 function Sidebar() {
   const location = useLocation();
   let active_path = location.pathname;
 
   const pages = {
-    "/": "Services",
-    "/p2p": "Peer-To-Peer Network",
-    "/email": "Email",
+    "/": <FaHouse size={"2rem"} />,
+    "/p2p": <AiFillMessage size={"2rem"} />,
+    "/email": <MdEmail size={"2rem"} />,
   };
-  console.log(active_path);
+
   return (
-    <div className="sidebar">
-      <h2>Hackaburg 2025</h2>
-      <div className="sidebar-pages">
-        {Object.entries(pages).map(([path, page]) => {
+    <>
+      <div className="topbar">
+        <h2 className="brand">Box</h2>
+        <div className="meta">
+          <span className="version">Version 0.1.5</span>
+          <span className="update-icon">
+            <FaCircleCheck size={"1.5rem"} />
+          </span>
+        </div>
+      </div>
+      <div className="page-bar">
+        {Object.entries(pages).map(([path, icon]) => {
           return (
             <Link
               to={path}
@@ -24,12 +37,12 @@ function Sidebar() {
                 "sidebar-link " + (active_path === path ? "active" : "")
               }
             >
-              {page}
+              {icon}
             </Link>
           );
         })}
       </div>
-    </div>
+    </>
   );
 }
 
